cfcdf40df1221043aa0b3a8331dbbd2c70a50ab6
Shabnam Rasoulian <Shabnam.rasoulian@gmail.com>
2018-11-21 12:28:46 -0500

    Create Transaction model and  associations

---
+++ server/db/models/transaction.js
@@ -0,0 +1,30 @@
+//const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Transaction = db.define('transaction', {
+  ticker: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1
+    }
+  },
+  tradeType: {
+    type: Sequelize.ENUM('buy', 'sell'),
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  }
+})
+
+module.exports = Transaction
