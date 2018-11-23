4fa7d468ef5c6104c2b9c8c707014e2495d31aed
Shabnam Rasoulian <Shabnam.rasoulian@gmail.com>
2018-11-22 21:32:29 -0500

    Modify index file for api routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/users', require('./users'))
+router.use('/portfolios', require('./portfolios'))
 router.use('/transactions', require('./transactions'))
+router.use('/users', require('./users'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
